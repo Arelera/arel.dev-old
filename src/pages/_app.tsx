f21b33b6e85f33a9e026c86140e78913d1434864
Arelera <arelcode@gmail.com>
2023-05-27 21:16:14 +0300

    updated ga4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Space_Mono } from 'next/font/google'
+import Script from 'next/script'
 
 const spaceMono = Space_Mono({
   weight: ['400'],
@@ -9,8 +10,24 @@ const spaceMono = Space_Mono({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={spaceMono.className}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_KEY}`}
+      ></Script>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', ${process.env.NEXT_PUBLIC_GA_KEY});
+          `,
+        }}
+      ></Script>
+      <main className={spaceMono.className}>
+        <Component {...pageProps} />
+      </main>
+    </>
   )
 }
