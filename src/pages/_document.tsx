e5b69e802600cb1da0c11293c9984e61b7f88669
Arelera <arelcode@gmail.com>
2023-05-27 19:18:53 +0300

    added pnpm step to actions, added ga4 script

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,23 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_KEY}`}
+      ></Script>
+      <Script>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GA_KEY});
+        `}
+      </Script>
       <body className="bg-black text-slate-300">
         <Main />
         <NextScript />
